50308d2da578210dc0720daf2d46e98257b369c0
-Kevin_ <t66b6g77@gmail.com>
2024-01-16 20:09:35 -0300

    Update the frontend and backend code

--- link_bio/link_bio/link_bio.py
+++ link_bio/link_bio/link_bio.py
@@ -1,3 +1,15 @@
 import reflex as rx
+from link_bio.components.navbar import navbar
+
+
+class State(rx.State):
+    pass
+
 
 def index() -> rx.Component:
+    return navbar()
+
+
+app = rx.App()
+app.add_page(index) 
+app.compile()
