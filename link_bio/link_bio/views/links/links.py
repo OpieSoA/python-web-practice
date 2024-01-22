7e70d9195f4acd776e1d5eacf6840ff04f4b947d
-Kevin_ <t66b6g77@gmail.com>
2024-01-22 13:56:10 -0300

    Update links.py

--- link_bio/link_bio/views/links/links.py
+++ link_bio/link_bio/views/links/links.py
@@ -5,8 +5,8 @@ from link_bio.components.link_button import link_button
 def links() -> rx.Component:
     return rx.vstack(
         link_button("Portfolio", "https://moure.dev"),
-        link_button("Github", "https://github.com/OpieSoA"),
-        link_button("Discord", "https://discord.gg/S2e8Repwb4"),
-        link_button("Linkedln", "https://www.linkedin.com/in/kevin-suasnabar"),
+        link_button("Github", "https://github.com/mouredev"),
+        link_button("Discord", "https://discord.gg/mouredev"),
+        link_button("Linkedln", "https://www.linkedin.com/in/braismoure/"),
         width="100%"
     )
