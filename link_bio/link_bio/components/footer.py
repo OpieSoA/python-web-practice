377f75b172cea8bb81417ab9e3a96d66e1e9e8e2
-Kevin_ <t66b6g77@gmail.com>
2024-01-22 13:59:15 -0300

    Update footer and header components

--- link_bio/link_bio/components/footer.py
+++ link_bio/link_bio/components/footer.py
@@ -5,9 +5,9 @@ import datetime
 def footer() -> rx.Component:
     return rx.vstack(
         rx.image(src="/favicon.ico", width="26px", height="auto"),
-        rx.link( f"© {datetime.date.today().year} by -Kevin_ v2.",
+        rx.link( f"© {datetime.date.today().year} by brais v2.",
         href="https://mouredev.com",
         is_external=True
         ),
-        rx.text("BUILDING SOFTWARE WITH ♥ FROM ARGENTINA TO THE WORLD.")
+        rx.text("BUILDING SOFTWARE WITH ♥ FROM GALICIA TO THE WORLD.")
         )
--- link_bio/link_bio/views/header/header.py
+++ link_bio/link_bio/views/header/header.py
@@ -3,8 +3,7 @@ import reflex as rx
 def header() -> rx.Component:
     return rx.vstack(
         rx.avatar(name="e", size="xl"),
-        rx.text("@kacsn7"),
-        rx.text("HOLA 👋 MI NOMBRE ES KEVIN SUASNABAR"),
-        rx.text("""Indie (Trainee) Fullstack Developer & 
-                Cybersecurity Student"""),
+        rx.text("@brais moure"),
+        rx.text("HOLA 👋 MI NOMBRE ES Tito"),
+        rx.text("""Fullstack Developer"""),
     )
